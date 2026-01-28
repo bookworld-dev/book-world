012306e46e57fa131fa9596992a8aa07d9a59edf
Ewan Sheldon <ewansheldon@gmail.com>
2026-01-28 16:46:39 +0100

    completes seeding script

--- scripts/seedLocations.ts
+++ scripts/seedLocations.ts
@@ -3,99 +3,62 @@ import { getDb } from '../app/lib/db.ts';
 import fs from 'node:fs';
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
+import { Location } from '@/app/lib/types.ts';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
-type CountryFeature = {
-  properties: {
-    ISO_A2: string;
-    ISO_A2_EH: string;
-    NAME: string;
-    adm1_code?: string;
-  };
-};
-
-type RegionFeature = {
-  properties: {
-    iso_a2: string;
-    name: string;
-    iso_3166_2: string;
-  };
-};
-
-type CountryRow = {
-    id: string;
-    level: string;
-    code: string;
-    name: string;
-    parent_id: string;
+type NewLocationRow = {
+  level: string;
+  code: string;
+  name: string;
 }
 
-const extractCode = (feature: CountryFeature): string => {
-  let code = feature.properties.ISO_A2;
-  if (code && code !== '-99') return code;
-  return feature.properties.ISO_A2_EH;
-}
+type CountryRow = NewLocationRow & { id: string; }
 
-const seedCountries = async (): Promise<CountryRow[]> => {
-  const data = JSON.parse(
-    fs.readFileSync(
-      path.join(__dirname, 'data/countries.geojson'),
-      'utf-8'
-    )
+const saveCountry = async (country: NewLocationRow) => {
+  const result = await getDb().query(
+    `
+    INSERT INTO locations (level, code, name)
+    VALUES ($1,$2,$3)
+    RETURNING *
+    `, [country.level, country.code, country.name]
   );
 
-  const countries = [];
+  return result.rows[0];
+}
 
-  for (const feature of data.features as CountryFeature[]) {
-    const code = extractCode(feature);
-    if (!code || code === '-99') {
-      continue;
-    }
-
-    const result = await getDb().query(
+const saveRegions = async (states: NewLocationRow[], parentId: string) => {
+  for (const state of states) {
+    await getDb().query(
       `
-        INSERT INTO locations (level, code, name, parent_id)
-        VALUES ('country', $1, $2, NULL)
-        RETURNING *
-      `,
-      [code, feature.properties.NAME]
+      INSERT INTO locations (level, code, name, parent_id)
+      VALUES ($1,$2,$3,$4)
+      RETURNING *
+      `, [state.level, state.code, state.name, parentId]
     );
-    countries.push(result.rows[0]);
   }
+}
 
-  return countries;
-};
-
-const seedAdmin1 = async (countries: CountryRow[]) => {
-  const data = JSON.parse(
+const seedLocations = async () => {
+  const data: NewLocationRow[] = JSON.parse(
     fs.readFileSync(
-      path.join(__dirname, 'data/admin1.geojson'),
+      path.join(__dirname, 'data/locations.json'),
       'utf-8'
     )
   );
+  
+  const countries = data.filter(l => l.level === 'country');
 
-  for (const feature of data.features as RegionFeature[]) {
-    const code = feature.properties.iso_3166_2;
-    if (code.includes('-99')) continue;
-    try {
-      const parentId = countries.filter(country => country.code === feature.properties.iso_a2)[0].id;
-      await getDb().query(
-        `
-          INSERT INTO locations (level, code, name, parent_id)
-          VALUES ('state', $1, $2, $3)
-        `,
-        [code, feature.properties.name, parentId]
-      );
-    } catch (e) {
-    }
+  for (const country of countries) {
+    const countryRow: CountryRow = await saveCountry(country)
+
+    const countryRegions = data.filter(l => l.level !== 'country' && l.code.split('-')[0] === country.code);
+    await saveRegions(countryRegions, countryRow.id);
   }
 }
 
 const main = async () => {
-  const countries = await seedCountries();
-  await seedAdmin1(countries)
+  await seedLocations();
 }
 
 main();
