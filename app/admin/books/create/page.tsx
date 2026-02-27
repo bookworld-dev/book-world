04a3fe27e1a78937955fc85b759feb7f591f3058
Ewan Sheldon <ewansheldon@gmail.com>
2026-02-27 14:54:58 +0100

    form to create new book

---
+++ app/admin/books/create/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+import { createBook } from "@/app/lib/book.api";
+import { useState } from "react";
+
+type NewBook = {
+  title: string;
+  author: string;
+  cover?: File;
+};
+
+const CreateBookForm = () => {
+  const [newBook, setNewBook] = useState<NewBook>({
+    title: '',
+    author: '',
+  });
+  
+  const previewUrl: string|undefined = newBook.cover ?
+    URL.createObjectURL(newBook.cover) : undefined;
+
+  const handleChange = (field: keyof NewBook) =>
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setNewBook(b => ({
+        ...b,
+        [field]: e.target.value,
+      }));
+    };
+
+  const handleCoverUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    setNewBook(b => ({
+      ...b,
+      cover: file,
+    }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!newBook.cover) return;
+    const formData = new FormData();
+    formData.append('title', newBook.title);
+    formData.append('author', newBook.author);
+    formData.append('cover', newBook.cover);
+    createBook(formData);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Title"
+        value={newBook.title}
+        onChange={handleChange('title')}
+        aria-label="book-title"
+        required
+      />
+
+      <input
+        type="text"
+        placeholder="Author"
+        value={newBook.author}
+        onChange={handleChange('author')}
+        aria-label="book-author"
+        required
+      />
+
+      <input
+        type="file"
+        accept="image/png, image/jpeg"
+        onChange={handleCoverUpload}
+        aria-label="book-cover"
+      />
+      
+      { previewUrl && 
+        <img
+          src={previewUrl}
+          alt="Cover preview"
+          style={{ width: 200 }}
+          onLoad={() => URL.revokeObjectURL(previewUrl)}
+        />
+      }
+      <button type="submit">Save</button>
+    </form>
+  );
+};
+
+export default CreateBookForm;
