8aa1f52ad429dd8a1336889bde9246b10b3fa890
Ewan Sheldon <ewansheldon@gmail.com>
2026-02-27 14:55:25 +0100

    create book lib function for api call

--- app/lib/book.api.ts
+++ app/lib/book.api.ts
@@ -4,3 +4,7 @@ import { Book } from './types';
 export const getRandomBookByLocation = async (location: string): Promise<Book> => {
   return apiFetch<Book>(`/api/books/random?location=${location}`);
 };
+
+export const createBook = async (book: FormData): Promise<Book> => {
+  return apiFetch<Book>(`/api/books`, { method: 'POST', body: book });
+}
